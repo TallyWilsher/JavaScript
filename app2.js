0ec5877ce72be971a6c6fe82c0e8a42fdaa215ae
Tally Wilsher <twilsher@1800contatcs.com>
2019-10-16 15:49:08 -0600

    wip

---
+++ app2.js
@@ -0,0 +1,60 @@
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(numOfSlices);
+console.log(favoriteFood );
+
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+
+
+
+// These console.log() statements below will help you check the values of the variables.
+// You do not need to edit these statements. 
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
+
+
+var favoriteAnimal = "dogo";
+console.log('My favorite animal: '+ favoriteAnimal);
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+console.log(gainedDollar);
+console.log(lostDollar);
+
+gainedDollar++;
+lostDollar--;
+
+console.log(gainedDollar);
+console.log(lostDollar);
+
+var myName = 'Tally';
+var myCity = 'New York City';
+
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable);
+
+newVariable = 1;
+
+console.log(typeof newVariable);
