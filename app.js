855a611ccbe322770551f1c1fdc6034c20640baf
Tally Wilsher <twilsher@1800contatcs.com>
2019-10-16 14:47:37 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// single-line comments 
+console.log();
+/*multi-line comment*/
+console.log();
+// strings, numbers, booleans, null, undefined, symbol, and object.
+console.log("string");
+console.log(27);
+console.log(true);
+console.log(null);
+console.log(undefined);
+// +, -, *, /, and %.
+console.log(Math.floor(2.6) + 2 * 3 / 4);
+console.log('This is how many chars long?'.length);
+console.log('why are you shouting!'.toUpperCase());
